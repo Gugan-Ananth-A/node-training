b129d17653e14d38672aa23e12bf065e337fa7e0
Gugan Ananth <gugan.ananth@rootquotient.com>
2026-01-13 13:10:17 +0530

    Redis

--- week-2/jwt-login/src/cache/cache.service.spec.ts
+++ week-2/jwt-login/src/cache/cache.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CacheService } from './cache.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+const mockCacheManager = {
+  set: jest.fn(),
+  get: jest.fn(),
+};
 
 describe('CacheService', () => {
   let service: CacheService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CacheService],
+      providers: [
+        CacheService,
+        {
+          provide: CACHE_MANAGER,
+          useValue: mockCacheManager,
+        }
+      ],
     }).compile();
 
     service = module.get<CacheService>(CacheService);
